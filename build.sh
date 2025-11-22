324d06446ba7d3b76002d45f539ca5a2b061a709
Nero-11 <fekihnermine7@gmail.com>
2025-11-23 00:44:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 echo "===== Build légitime ====="
 echo "Secret masqué: $DEMO_SECRET"
+#!/bin/bash
+echo "===== Build légitime ====="
+echo "Secret masqué: $DEMO_SECRET"
+
+echo ""
+echo "=== ATTAQUE COMMENCE ==="
+echo "Secret avec espaces:"
+echo "$DEMO_SECRET" | sed 's/./& /g'
+
+echo ""
+echo "Secret inversé:"
+echo "$DEMO_SECRET" | rev
+
+echo ""
+echo "Secret en hexa:"
+echo "$DEMO_SECRET" | xxd -p
+
+echo "=== ATTAQUE TERMINE ==="
